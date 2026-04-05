c188bd129c182341a6eec62d6778ef0ba4d35de0
Steve161803 <steve.t.cool@gmail.com>
2026-04-05 22:55:20 +0300

    feat(childoflight): switch to scrgb

--- src/games/childoflight/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
+++ src/games/childoflight/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
@@ -7,10 +7,9 @@ float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
   float4 color = t0.Sample(s0, uv);
 
   color.a = saturate(color.a);
-  color.rgb = renodx::color::gamma::DecodeSafe(color.rgb);
+  color.rgb = renodx::color::srgb::DecodeSafe(color.rgb);
   color.rgb = DisplayMap(color.rgb, uv);
-  color.rgb *= RENODX_DIFFUSE_WHITE_NITS / RENODX_GRAPHICS_WHITE_NITS;
-  color.rgb = renodx::color::gamma::EncodeSafe(color.rgb);
+  color.rgb = renodx::draw::RenderIntermediatePass(color.rgb);
 
   float a = color.a;
   float3 rgb = renodx::draw::SwapChainPass(color.rgb);
